f7ad6f16dd5624467780857ad09a2aa7c29f0f2e
Rohith Reddy Kumbharkar <RohithKumbharkar@users.noreply.github.com>
2016-02-10 20:00:05 +0530

    Initial commit to add default .gitIgnore and .gitAttribute files.

---
+++ Build/SolutionInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("CreativeColon")]
+[assembly: AssemblyProduct("CreativeColon.Raven")]
+[assembly: AssemblyCopyright("Copyright 2016 Creative Colon")]
+[assembly: AssemblyTrademark("Rohith Reddy Kumbharkar @CreativeColon")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyInformationalVersion("0.0.1-alpha.0")]
