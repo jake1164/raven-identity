eddbaf999cb0230ba133ec8d5136b5cb256094fd
Rohith Reddy Kumbharkar <RohithKumbharkar@users.noreply.github.com>
2016-02-11 09:20:01 +0530

    Add full blown MVC template with Individual User Accounts

---
+++ Web.MVC.Full/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web.MVC.Full.MvcApplication" Language="C#" %>
---
+++ Web.MVC.Full/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web.MVC.Full.Startup))]
+namespace Web.MVC.Full
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
