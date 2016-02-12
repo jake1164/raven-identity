d3be5956a26f1a9b6500b1d25145e554272a8a25
Rohith Reddy Kumbharkar <RohithKumbharkar@users.noreply.github.com>
2016-02-12 23:49:36 +0530

    Added full blown MVC template with Individual User Accounts

---
+++ Web.MVC.Minimal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web.MVC.Minimal.Startup))]
+namespace Web.MVC.Minimal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
